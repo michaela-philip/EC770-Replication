960686edecaf8d60b5c7e5a2b7b13a7c2ab09ecd
Michaela <michaela.philip@emory.edu>
2023-10-16 17:11:25 -0400

    load code for full sample

---
+++ import 2002.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+#specifying column breakdowns for 2002 data
+colspecs = [(0, 4), (4, 41), (41, 43), (43, 46), (46, 57), (57, 58), (58, 69), (69, 71), (71, 76), (76, 77), (77, 79), (79, 81), (81, 84), (84, 85), (85, 86), (86, 87), (87, 95), (95, 96), (96, 108), (108, 109), (109, 171), (171, 173), (173, 182), (182, 184), (184, 187), (187, 189), (189, 192), (192, 196), (196, 198), (198, 199), (199, 200), (200, 201), (201, 241), (241, 242), (242, -1)]
+
+#trying to read in the data using fwf
+natl2002 = pd.read_fwf("C:/Users/micha/OneDrive - Emory University/Second Year/Health Econ I/Replication/Data/Natl2001.dat", colspecs = colspecs, header = None)
+
+natl2002.columns = ['year', 'drop0', 'state', 'county', 'drop1', 'countysize', 'drop2', 'age', 'drop3', 'hisp', 'drop4', 'race', 'drop5', 'educ', 'drop6', 'marital', 'drop7', 'priorchild', 'drop8', 'prenatal', 'drop9', 'month', 'drop10', 'gestation', 'drop11', 'sex', 'drop12', 'birthweight', 'drop13', 'birthcat', 'drop14', 'number', 'drop15', 'tobacco', 'drop16']
+
+#dropping unneeded columns
+nat02 = natl2002.drop(columns=['drop0', 'drop1', 'drop2', 'drop3', 'drop4', 'drop5', 'drop6', 'drop7', 'drop8', 'drop9', 'drop10', 'drop11', 'drop12', 'drop13', 'drop14', 'drop15', 'drop16'])
