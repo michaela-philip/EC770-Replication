0560d76b0c7e2d83a0f0592ee8a6b1fb6efa5c91
Michaela <michaela.philip@emory.edu>
2023-10-21 16:47:23 -0400

    removed limit from load

--- load01.py
+++ load01.py
@@ -8,7 +8,7 @@ def main():
     columns = ['year', 'drop0', 'state', 'county', 'drop1', 'countysize', 'drop2', 'age', 'drop3', 'hisp', 'drop4', 'race', 'drop5', 'educ', 'drop6', 'marital', 'drop7', 'priorchild', 'drop8', 'prenatal', 'drop9', 'month', 'drop10', 'gestation', 'drop11', 'sex', 'drop12', 'birthweight', 'drop13', 'birthcat', 'drop14', 'number', 'drop15', 'tobacco', 'drop16']
     drop_columns = ['drop0', 'drop1', 'drop2', 'drop3', 'drop4', 'drop5', 'drop6', 'drop7', 'drop8', 'drop9', 'drop10', 'drop11', 'drop12', 'drop13', 'drop14', 'drop15', 'drop16']
 
-    nat01 = import_data(filepath, colspecs, columns, drop_columns, 100)
+    nat01 = import_data(filepath, colspecs, columns, drop_columns)
     print(nat01.head())
 
 if __name__ == "__main__":
