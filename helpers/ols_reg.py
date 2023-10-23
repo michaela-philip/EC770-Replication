39bb7767fa2a0ab5974c582b9f2eaee5cc2af3bc
Michaela <michaela.philip@emory.edu>
2023-10-23 16:13:38 -0400

    typo

--- helpers/ols_reg.py
+++ helpers/ols_reg.py
@@ -4,7 +4,7 @@ import statsmodels.api as sm
 from patsy import dmatrices 
 
 def ols_reg(data, y_var, x_var):
-    formula = y_var + '~' + '+' .join(x_var) + ' + C(state) + C(year)'
+    formula = y_var + '~' + '+' .join(x_var) + ' + C(state_fips) + C(year)'
     y, X = dmatrices(formula, data, return_type = 'dataframe')
     model = sm.OLS(y, X, missing = 'drop').fit()
     return model
