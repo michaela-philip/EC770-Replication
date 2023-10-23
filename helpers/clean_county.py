8908dc63c83c59ed491441fcceb576b27244b5f1
Michaela <michaela.philip@emory.edu>
2023-10-23 10:26:46 -0400

    rural unemployment * 100

--- helpers/clean_county.py
+++ helpers/clean_county.py
@@ -6,6 +6,8 @@ def clean_county(filepath_u, filepath_r):
 
     output_2 = pd.read_stata(filepath_r)
 
+    output_2['unemp'] = output_2['unemp']*100
+
     #we want to use rural county data for any county code 999
     output_2['cofips'] = pd.Series([999 for x in range(len(output_2.index))])
 
@@ -15,4 +17,9 @@ def clean_county(filepath_u, filepath_r):
     output = pd.concat([output_1, output_2])
 
     return output
+
+# filepath_u = "./inputs/county_vars.dta"
+# filepath_r = "./inputs/ruralcounty_vars.dta"
+
+# county = clean_county(filepath_u, filepath_r)
 
