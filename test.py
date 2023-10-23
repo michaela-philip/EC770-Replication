cb83b56b6b89399f0c56e664986796280d9c2284
Michaela <michaela.philip@emory.edu>
2023-10-23 12:31:29 -0400

    change dtype of states

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ def test_6():
     print("2003 import complete", nat_03.info())
 
     state_abbrev_fips_dict = { 'AL': '01', 'AK': '02', 'AZ': '04', 'AR': '05', 'CA': '06', 'CO': '08', 'CT': '09', 'DE': '10', 'FL': '12', 'GA': '13', 'HI': '15', 'ID': '16', 'IL': '17', 'IN': '18', 'IA': '19', 'KS': '20', 'KY': '21', 'LA': '22', 'ME': '23', 'MD': '24', 'MA': '25', 'MI': '26', 'MN': '27', 'MS': '28', 'MO': '29', 'MT': '30', 'NE': '31', 'NV': '32', 'NH': '33', 'NJ': '34', 'NM': '35', 'NY': '36', 'NC': '37', 'ND': '38', 'OH': '39', 'OK': '40', 'OR': '41', 'PA': '42', 'RI': '44', 'SC': '45', 'SD': '46', 'TN': '47', 'TX': '48', 'UT': '49', 'VT': '50', 'VA': '51', 'WA': '53', 'WV': '54', 'WI': '55', 'WY': '56', 'AS': '60', 'GU': '66', 'MP': '69', 'PR': '72', 'VI': '78'}
-    nat_03['state'] = nat_03['state'].map(state_abbrev_fips_dict)
+    nat_03['state'] = nat_03['state'].map(state_abbrev_fips_dict).int64() 
     print("state conversion complete", nat_03.info())
 
     cleaned_03 = clean_data(nat_03)
