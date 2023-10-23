656d796622b1fab8ad4560aefc0e115a96db9390
Michaela <michaela.philip@emory.edu>
2023-10-23 16:17:06 -0400

    remove intercept and drop

--- helpers/ols_reg.py
+++ helpers/ols_reg.py
@@ -4,7 +4,7 @@ import statsmodels.api as sm
 from patsy import dmatrices 
 
 def ols_reg(data, y_var, x_var):
-    formula = y_var + '~' + '+' .join(x_var) + ' + C(stfips) + C(year)'
+    formula = y_var + '~' + '+' .join(x_var) + ' + C(stfips) + C(year) - 1'
     y, X = dmatrices(formula, data, return_type = 'dataframe')
-    model = sm.OLS(y, X, missing = 'drop').fit()
+    model = sm.OLS(y, X).fit()
     return model
