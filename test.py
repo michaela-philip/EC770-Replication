a9ae77d7ec440edf4a4fe42ee468b527235edad6
Michaela <michaela.philip@emory.edu>
2023-10-23 09:45:38 -0400

    test 4 for county matching

--- test.py
+++ test.py
@@ -57,6 +57,25 @@ def test_3():
     nat_03 = import_data(filepath, colspecs, columns, drop_columns)
     print("2003 import complete")
 
+def test_4():
+    filepath = "./inputs/Natl2001.dat"
+    colspecs = [(0, 4), (4, 41), (41, 43), (43, 46), (46, 57), (57, 58), (58, 69), (69, 71), (71, 76), (76, 77), (77, 79), (79, 81), (81, 84), (84, 85), (85, 86), (86, 87), (87, 95), (95, 96), (96, 108), (108, 109), (109, 171), (171, 173), (173, 182), (182, 184), (184, 187), (187, 189), (189, 192), (192, 196), (196, 198), (198, 199), (199, 200), (200, 201), (201, 241), (241, 242), (242, -1)]
+    columns = ['year', 'drop0', 'state', 'county', 'drop1', 'countysize', 'drop2', 'age', 'drop3', 'hisp', 'drop4', 'race', 'drop5', 'educ', 'drop6', 'marital', 'drop7', 'priorchild', 'drop8', 'prenatal', 'drop9', 'month', 'drop10', 'gestation', 'drop11', 'sex', 'drop12', 'birthweight', 'drop13', 'birthcat', 'drop14', 'number', 'drop15', 'tobacco', 'drop16']
+    drop_columns = ['drop0', 'drop1', 'drop2', 'drop3', 'drop4', 'drop5', 'drop6', 'drop7', 'drop8', 'drop9', 'drop10', 'drop11', 'drop12', 'drop13', 'drop14', 'drop15', 'drop16']
+    nat_01 = import_data(filepath, colspecs, columns, drop_columns)
+    print("2001 import complete", nat_01.info())
+
+    cleaned_01 = clean_data(nat_01)
+
+    filepath_u = "./inputs/county_vars.dta"
+    filepath_r = "./inputs/ruralcounty_vars.dta"
+    county = clean_county(filepath_u, filepath_r)
+    print("county data cleaning complete")
+
+    test_birth_county = pd.merge(cleaned_01, county, on = ['cofips', 'stfips', 'year'], how = 'outer')
+
+    print(test_birth_county['cofips'].info())
+
 
 if __name__ == "__main__":
     print("I'm running!")
