99a9a090f78b38588de339c9c009d91ec367c5fc
Michaela <michaela.philip@emory.edu>
2023-10-23 11:20:59 -0400

    main function

--- regressions.py
+++ regressions.py
@@ -7,8 +7,15 @@ from helpers.ols_reg import ols_reg
 
 complete_data = pd.read_pickle("./complete_data.pkl")
 
-data = complete_data
-y_var = 'prenatal'
-x_var = ['eitc', 'age', 'marital', 'female', 'black', 'namer', 'asian', 'hisp', 'hseduc', 'hispmiss', 'cp500', 'cp250', 'cp100', 'cpsmall', 'unemp', 'rpcinc', 'pctpoverty', 'supplyMD_pc']
+def main():
 
-model_2_1 = ols_reg(data, y_var, x_var)
+    data = complete_data
+    y_var = 'prenatal'
+    x_var = ['eitc', 'age', 'marital', 'female', 'black', 'namer', 'asian', 'hisp', 'hseduc', 'hispmiss', 'cp500', 'cp250', 'cp100', 'cpsmall', 'unemp', 'rpcinc', 'pctpoverty', 'supplyMD_pc']
+
+    model_2_1 = ols_reg(data, y_var, x_var)
+
+
+if __name__ == "__main__":
+    print("I'm running!")
+    main()
