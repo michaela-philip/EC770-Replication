6389859ecc778e96eeb9de0d7b1b46a0985c69f7
Michaela <michaela.philip@emory.edu>
2023-10-23 11:49:00 -0400

    remove constant from regression

--- helpers/ols_reg.py
+++ helpers/ols_reg.py
@@ -5,7 +5,6 @@ import statsmodels.api as sm
 def ols_reg(data, y_var, x_var):
     y = data[y_var]
     X = data[x_var]
-    X = sm.add_constant(X)
     model = sm.OLS(y, X).fit()
     return model
     print(model.summary())
