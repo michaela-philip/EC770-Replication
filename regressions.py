8afb7de90f6b14a6b237f7b40ed8c07d8da8c38a
Michaela <michaela.philip@emory.edu>
2023-10-23 12:00:13 -0400

    drop missing

--- regressions.py
+++ regressions.py
@@ -13,7 +13,7 @@ def main():
     y_var = 'prenatal'
     x_var = ['eitc', 'age', 'marital', 'female', 'black', 'namer', 'asian', 'hisp', 'hseduc', 'hispmiss', 'cp500', 'cp250', 'cp100', 'cpsmall', 'unemp', 'rpcinc', 'pctpoverty', 'supplyMD_pc']
 
-    model_2_1 = ols_reg(data, y_var, x_var)
+    model_2_1 = ols_reg(data, y_var, x_var, missing = 'drop')
 
 
 if __name__ == "__main__":
