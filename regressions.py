6c56bd675644557b34f70a962a5f9a46c6f564e9
Michaela <michaela.philip@emory.edu>
2023-10-23 11:52:23 -0400

    adjust x and y value call

--- regressions.py
+++ regressions.py
@@ -10,8 +10,8 @@ complete_data = pd.read_pickle("./complete_data.pkl")
 def main():
 
     data = complete_data
-    y_var = 'prenatal'
-    x_var = ['eitc', 'age', 'marital', 'female', 'black', 'namer', 'asian', 'hisp', 'hseduc', 'hispmiss', 'cp500', 'cp250', 'cp100', 'cpsmall', 'unemp', 'rpcinc', 'pctpoverty', 'supplyMD_pc']
+    y_var = prenatal
+    x_var = [eitc, age, marital, female, black, namer, asian, hisp, hseduc, hispmiss, cp500, cp250, cp100, cpsmall, unemp, rpcinc, pctpoverty, supplyMD_pc]
 
     model_2_1 = ols_reg(data, y_var, x_var)
 
