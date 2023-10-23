a1ae97bee44dcab1fcf2b32d0e3a6b92e5659fc7
Michaela <michaela.philip@emory.edu>
2023-10-23 16:49:28 -0400

    add coefficients to table

--- tables.py
+++ tables.py
@@ -14,12 +14,16 @@ def main():
     table_1 = summary_stats(complete_data)
     print(table_1)
 
+    table_2 = pd.DataFrame()
+
     data = complete_data
 
     y_var = 'prenatal'
     x_var = ['eitc', 'age', 'marital', 'female', 'black', 'namer', 'asian', 'hisp', 'hseduc', 'hispmiss', 'cp500', 'cp250', 'cp100', 'cpsmall', 'unemp', 'rpcinc', 'pctpoverty', 'supplyMD_pc']
     model_2_1 = ols_reg(data, y_var, x_var)
-    print(model_2_1.summary())
+    # print(model_2_1.summary())
+    table_2['1st Trimester Prenatal Care'] = model_2_1.params()
+    print(table_2)
 
     # y_var = 'tobacco'
     # model_2_2 = ols_reg(data, y_var, x_var)
