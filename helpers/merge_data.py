d98123931605f41d9c05d60ff379b08aba7180a7
Michaela <michaela.philip@emory.edu>
2023-10-24 07:48:03 -0400

    merge eitc on 'inner'

--- helpers/merge_data.py
+++ helpers/merge_data.py
@@ -7,7 +7,7 @@ def merge_data(birth_1, birth_2, birth_3, county, eitc):
 
     birth_county = pd.merge(births, county, on = ['cofips', 'stfips', 'year'], how = 'inner')
 
-    birth_county_eitc = pd.merge(birth_county, eitc, on = ['stfips', 'year', 'numchildren'], how = 'outer')
+    birth_county_eitc = pd.merge(birth_county, eitc, on = ['stfips', 'year', 'numchildren'], how = 'inner')
 
     # #changing order of columns for my sanity
     # birth_county_eitc.set_index(birth_county_eitc.pop('year'), inplace=True)
