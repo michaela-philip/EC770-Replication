d424e3f410835b3110e46b19dbac08789e3906f8
Michaela <michaela.philip@emory.edu>
2023-10-25 09:39:27 -0400

    change name of first merge function

--- helpers/merge_data.py
+++ helpers/merge_data.py
@@ -1,31 +1,23 @@
 import numpy as np
 import pandas as pd
 
-# def merge_data(birth_1, birth_2, birth_3, county, eitc):
-#     #first we combine all the birth data
-#     births = pd.concat([birth_1, birth_2, birth_3])
+def merge_data_2(birth_1, birth_2, birth_3, county, eitc):
+    #first we combine all the birth data
+    births = pd.concat([birth_1, birth_2, birth_3])
 
-#     birth_county = pd.merge(births, county, on = ['cofips', 'stfips', 'year'], how = 'inner')
+    birth_county = pd.merge(births, county, on = ['cofips', 'stfips', 'year'], how = 'inner')
 
-#     birth_county_eitc = pd.merge(birth_county, eitc, on = ['stfips', 'year', 'numchildren'], how = 'inner')
+    birth_county_eitc = pd.merge(birth_county, eitc, on = ['stfips', 'year', 'numchildren'], how = 'inner')
 
-#     # #changing order of columns for my sanity
-#     # birth_county_eitc.set_index(birth_county_eitc.pop('year'), inplace=True)
-#     # birth_county_eitc.reset_index(inplace=True)
-
-#     return birth_county_eitc
+    return birth_county_eitc
 
 def merge_data(birth_1, birth_2, birth_3, county, eitc):
     #first we combine all the birth data
-    births = pd.concat([birth_1, birth_2, birth_3])
+    births = pd.concat([birth_1, birth_2, birth_3])    
 
     county_eitc = pd.merge(county, eitc, on = ['stfips', 'year'], how = 'inner')
 
     birth_county_eitc = pd.merge(births, county_eitc, on = ['stfips', 'year', 'numchildren'], how = 'inner')
-
-    # #changing order of columns for my sanity
-    # birth_county_eitc.set_index(birth_county_eitc.pop('year'), inplace=True)
-    # birth_county_eitc.reset_index(inplace=True)
 
     return birth_county_eitc
 
