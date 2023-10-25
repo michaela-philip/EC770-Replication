4037594a175245c860de59b7b0b78a927cc1646f
Michaela <michaela.philip@emory.edu>
2023-10-25 10:05:04 -0400

    missing + in formula

--- helpers/ols_reg.py
+++ helpers/ols_reg.py
@@ -5,7 +5,7 @@ from patsy import dmatrices
 
 def ols_reg(data, y_var, x_var):
     data = data.dropna(subset = [y_var] + x_var)
-    formula = y_var + '~' + '+' .join(x_var) + 'C(stfips) + C(year) - 1'
+    formula = y_var + '~' + '+' .join(x_var) + '+ C(stfips) + C(year) - 1'
     y, X = dmatrices(formula, data, return_type = 'dataframe')
     model = sm.OLS(y, X).fit(cov_type = 'cluster', cov_kwds = {'groups': data['stfips']})
     return model
