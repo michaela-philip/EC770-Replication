91e5d890a0cd2c50c486ca5507bab2503d250496
Michaela <michaela.philip@emory.edu>
2023-10-25 22:58:20 -0400

    troubleshoot

--- question_2_c.py
+++ question_2_c.py
@@ -13,7 +13,9 @@ model_5_1 = sm.Probit(y, X).fit(cov_type = 'cluster', cov_kwds = {'groups': data
 print(model_5_1.summary())
 
 y_var = 'lbw'
+x_var = ['age', 'marital', 'female', 'black', 'namer', 'asian', 'hisp', 'hseduc', 'hispmiss', 'cp500', 'cp250', 'cp100', 'cpsmall', 'unemp', 'rpcinc', 'pctpoverty', 'supplyMD_pc', 'eitc']
 formula = y_var + '~' + '+' .join(x_var) + '+ C(stfips) + C(year) - 1'
+y, X = dmatrices(formula, data, return_type = 'dataframe')
 model_5_2 = sm.Probit(y, X).fit(cov_type = 'cluster', cov_kwds = {'groups': data['stfips']})
 print(model_5_2.summary())
 
@@ -24,7 +26,7 @@ lbw_me = model_5_2.get_margeff(at='overall', method='dydx', dummy=True)
 print(prenatal_me.summary())
 print(lbw_me.summary())
 
-data={'prenatal' : model_5_1.params, 'prenatal se' : model_5_1.bse, 'low birth weight' : model_5_2.params, 'low birth weight se' : model_5_2.bse}
+# data={'prenatal' : model_5_1.params, 'prenatal se' : model_5_1.bse, 'low birth weight' : model_5_2.params, 'low birth weight se' : model_5_2.bse}
 
 # table_5 = pd.DataFrame(data, index = x_var)
 # table_6 = pd.DataFrame(prenatal_me.summary_frame(), index = x_var)
